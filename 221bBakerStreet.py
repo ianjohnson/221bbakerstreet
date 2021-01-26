2933b9d198e0bf71c21c1fb453b79cb25a6e1139
Ian Johnson <ianjohnson@postmaster.co.uk>
2021-01-26 17:27:12 +0000

    More stuff

--- 221bBakerStreet.py
+++ 221bBakerStreet.py
@@ -145,6 +145,10 @@ class DestinationSquare(Square):
   def __str__(self):
     return "D□{" + self.group + "}<" + ", ".join([str(a) for a in self.actions]) + ">[%d,%d]" % (self.x, self.y)
 
+class MultiDestinationSquare(DestinationSquare):
+  def __init__(self, x, y, group):
+    super(MultiDestinationSquare, self).__init__(x, y, group)
+
 class StartSquare(Square):
   def __init__(self, x, y):
     super(StartSquare, self).__init__(x, y)
@@ -183,10 +187,12 @@ class BakerStreetBoard(object):
           actions.append(Square(x, y))
         elif square.startswith('D'):
           group_id = square[1:]
-          if not square[1:]:
+          if not group_id:
             group_id = str(BakerStreetBoard.__GROUP_ID)
             BakerStreetBoard.__GROUP_ID += 1
-          destination = DestinationSquare(x, y, group_id)
+            destination = DestinationSquare(x, y, group_id)
+          else:
+            destination = MultiDestinationSquare(x, y, group_id)
           if destination.group in destinations:
             destinations[destination.group]['squares'].append(destination)
           else:
@@ -213,6 +219,7 @@ class BakerStreetBoard(object):
     self.__start_square = ss
     self.__destinations = ds
     self.__current_square = ss
+    self.__leaving_destination = False
 
   def initialise(self):
     self.__current_square = self.__start_square
@@ -243,16 +250,21 @@ class BakerStreetBoard(object):
 
   def next_square(self, action_class):
     # Park and Docks have multiple entrances/exits. Choose randomly how to leave.
-    if isinstance(self.__current_square, DestinationSquare) and \
-       len(self.__destinations[self.__current_square.group]['squares']) > 1:
-      idx = random.randint(0, len(self.__destinations[self.__current_square.group]['squares']) - 1)
-      self.__current_square = self.__destinations[self.__current_square.group]['squares'][idx].next_square(action_class)
+    if isinstance(self.__current_square, MultiDestinationSquare):
+      if self.__leaving_destination:
+        next_square = self.__current_square.next_square(action_class)
+        if next_square != self.__current_square:
+          self.__current_square = next_square
+          self.__leaving_destination = False
+      else:
+        idx = random.randint(0, len(self.__destinations[self.__current_square.group]['squares']) - 1)
+        self.__current_square = self.__destinations[self.__current_square.group]['squares'][idx]
+        self.__leaving_destination = True
     else:
       self.__current_square = self.__current_square.next_square(action_class)
-
-    # Record a visit to a destination square
-    if isinstance(self.__current_square, DestinationSquare):
-      self.__destinations[self.__current_square.group]['visited'] = True
+      # Record a visit to a destination square
+      if self.__current_square.is_destination:
+        self.__destinations[self.__current_square.group]['visited'] = True
 
     return self.__current_square
 
@@ -289,10 +301,10 @@ class BakerStreetEnvironment(object):
     current_square = self.__board.current_square
     next_square = self.__board.next_square(action)
     terminal = self.__board.all_destinations_visited and next_square == self.__board.start_square
-    reward = 1 if terminal else -1 #if next_square not in self.__visited_squares else -2
+    reward = 100 if terminal else -1 if next_square not in self.__visited_squares else -2
     self.__visited_squares.add(next_square)
     if current_square == next_square:
-      reward = -100
+      reward = -1000
     return next_square, terminal, reward
 
   def render(self):
@@ -301,13 +313,13 @@ class BakerStreetEnvironment(object):
         if col:
           print("□", end = '')
           if isinstance(col, DestinationSquare):
-            print(" {%2s} " % col.group[0:2], end = '')
+            print(" {%2s}" % col.group[0:2], end = '')
           elif isinstance(col, StartSquare):
-            print(" S    ", end = '')
+            print(" ⌂   ", end = '')
           else:
-            print("      ", end = '')
+            print("     ", end = '')
         else:
-          print("====== ", end = '')
+          print("======", end = '')
       print()
 
 
@@ -324,63 +336,128 @@ def show_actions(Q, environment):
         action = np.argmax(values)
         klass = environment.actions[action]
         if klass == North:
-          print("N ", end = '')
+          print("↑ ", end = '')
         elif klass == East:
-          print("E ", end = '')
+          print("→ ", end = '')
         elif klass == South:
-          print("S ", end = '')
+          print("↓ ", end = '')
         else:
-          print("W ", end = '')
+          print("← ", end = '')
       else:
         print("  ", end = '')
     print()
 
 if __name__ == '__main__':
+  import argparse
+  import os
+
+  default_no_trials = 50000
+  default_no_exploration_trials = 45000
+  default_max_no_steps = 1e4
+  default_every = 1000
+  default_learning_rate = 1e-3
+  default_discount = 1.0 - (1.0 / default_max_no_steps)
+  default_epsilon = 1.0
+
+  parser = argparse.ArgumentParser(prog = "221bBakerStreet",
+                                   description = "221b Baker Street board route finder")
+  parser.add_argument('--trials', '-t',
+                      dest = 'no_trials',
+                      type = int,
+                      default = default_no_trials,
+                      help = "Number of trials (default: %s)" % default_no_trials)
+  parser.add_argument('--etrials', '-e',
+                      dest = 'no_exploration_trials',
+                      type = int,
+                      default = default_no_exploration_trials,
+                      help = "Number of exploration trials (default: %s)" % default_no_exploration_trials)
+  parser.add_argument('--steps', '-s',
+                      dest = 'max_no_steps',
+                      type = int,
+                      default = default_max_no_steps,
+                      help = "Maximum number of steps in a trial (default: %s)" % default_max_no_steps)
+  parser.add_argument('--every', '-v',
+                      dest = 'every_trials',
+                      type = int,
+                      default = default_every,
+                      help = "Display results every N trials (default: %s)" % default_every)
+  parser.add_argument('--learningrate', '-r',
+                      dest = 'learning_rate',
+                      type = float,
+                      default = default_learning_rate,
+                      help = "Learning rate (default: %.3f)" % default_learning_rate)
+  parser.add_argument('--discount', '-d',
+                      dest = 'discount',
+                      type = float,
+                      default = default_discount,
+                      help = "Discount (default: %.6f)" % default_discount)
+  parser.add_argument('--epsilon', '-p',
+                      dest = 'epsilon',
+                      type = float,
+                      default = default_epsilon,
+                      help = "Initial exploration (default: %.3f)" % default_epsilon)
+  args = parser.parse_args()
+
   board = BakerStreetBoard(baker_street_board)
   environment = BakerStreetEnvironment(board)
   environment.render()
-
-  # Learning parameters
-  ALPHA = 1e-3
-  GAMMA = 1.0
-  EPS = 1.0
 
   Q = {}
   for square in [item for sublist in environment.states for item in sublist if item]:
     for action in environment.actions:
       Q[square, action] = random.uniform(-1.0, 1.0)
 
-  no_epochs = 50000
-  every = 1000
-  total_rewards = np.zeros(no_epochs)
-  for i in range(no_epochs):
+  # Unpack parameters
+  alpha = args.learning_rate
+  discount = args.discount
+  epsilon = args.epsilon
+  no_trials = args.no_trials
+  no_exploration_epochs = args.no_exploration_trials
+  every = args.every_trials
+  max_no_steps = args.max_no_steps
+
+  total_rewards = []
+  plt.gcf().canvas.set_window_title("221b Baker Street Board Route Finder (pid = %d)" % os.getpid())
+  plt.xlabel("Trial Number")
+  plt.ylabel("Trial Reward")
+  plt.title("Trial Rewards")
+  plt.plot(total_rewards, linewidth = 1, color = 'blue')
+  plt.pause(0.05)
+  for i in range(no_trials):
     if i % every == 0:
       show_actions(Q, environment)
       print('Epoch: ', i, end = '')
     finished = False
     epoch_rewards = 0
     current_square = environment.reset()
+    no_steps = 0
     while not finished:
       rand = np.random.random()
-      action = max_action(Q, current_square, environment.actions) if rand < (1-EPS) else environment.random_action()
+      action = max_action(Q, current_square, environment.actions) if rand < (1-epsilon) else environment.random_action()
       new_square, finished, reward = environment.execute(action)
       epoch_rewards += reward
       new_action = max_action(Q, new_square, environment.actions)
-      Q[current_square, action] += ALPHA*(reward + GAMMA*Q[new_square, new_action] - Q[current_square, action])
+      Q[current_square, action] += alpha*(reward + discount*Q[new_square, new_action] - Q[current_square, action])
       current_square = new_square
+      no_steps += 1
+      if no_steps > max_no_steps:
+        finished = True
 
-    if EPS - (4.0 / (3.0 * no_epochs)) > 0:
-      EPS -= 4.0 / (3.0 * no_epochs)
+    if epsilon - (1.0 / no_exploration_epochs) > 0:
+      epsilon -= 1.0 / no_exploration_epochs
     else:
-      EPS = 0
+      epsilon = 0
 
     if i % every == 0:
       print(" (%.2f)" % epoch_rewards)
-    total_rewards[i] = epoch_rewards
+      plt.plot(total_rewards, linewidth = 1, color = 'blue')
+      plt.gcf().canvas.draw_idle()
+      plt.gcf().canvas.start_event_loop(0.05)
+    total_rewards.append(epoch_rewards)
 
   print("\nEnd")
   print("%.2f" % epoch_rewards)
   show_actions(Q, environment)
 
-  plt.plot(total_rewards)
+  plt.plot(total_rewards, linewidth = 1)
   plt.show()
